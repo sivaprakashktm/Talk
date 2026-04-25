6fdb0a31f27665c6604b86b44dc999172e7cb300
myspaceapp <myspaceappservice@gmail.com>
2026-04-25 21:48:36 +0530

    Welcome Name

--- Talk.WebUI/talk.app/src/App.tsx
+++ Talk.WebUI/talk.app/src/App.tsx
@@ -1,5 +1,8 @@
 import { Talk } from './TalkComponents/TalkComponent/Talk'
 
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import './App.css'
 
 function App() {
--- Talk.WebUI/talk.app/src/TalkComponents/WelcomeComponent/Welcome.tsx
+++ Talk.WebUI/talk.app/src/TalkComponents/WelcomeComponent/Welcome.tsx
@@ -2,10 +2,19 @@ import { useEffect, useState } from 'react'
 import './Welcome.css'
 
 export function Welcome() {
-  const [content, setContent] = useState('')
+  const [content, setContent] = useState<string>('')
+  const [welcomeNamePrefix, setWelcomeNamePrefix] = useState<string>('')
+  const [welcomeName, setWelcomeName] = useState<string>('')
+  const [welcomeNamePlaceHoder, setWelcomeNamePlaceHolder] = useState<string>('')
+
+  const changeWelcomeName = (event : React.ChangeEvent<HTMLInputElement, HTMLInputElement>) : void => {
+    setWelcomeName(event.target.value)
+  }
 
   useEffect(() => {
     setContent('Welcome to Talk!')
+    setWelcomeNamePrefix('Hi! ')
+    setWelcomeNamePlaceHolder('Your Name!')
   })
 
   return (
@@ -13,6 +22,15 @@ export function Welcome() {
       <div className='welcomeComponent'>
         <div className='welcomeContent'>
           {content}
+        </div>
+        <div className='welcomeNameContainer'>
+          <div className='welcomeNameContent'>
+            {welcomeNamePrefix + ' ' + welcomeName}
+          </div>
+          <div className='welcomeNameInput'>
+            <input className='welcomeInput form-control shadow' onChange={(event) => changeWelcomeName(event)} placeholder={welcomeNamePlaceHoder}>
+            </input>
+          </div>
         </div>
       </div>
     </>
